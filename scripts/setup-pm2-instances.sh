988aefba108faab6ad99ac3dcb296f88747548ed
Yoones Khoshghadam <yooneskh@gmail.com>
2024-10-03 16:29:34 +0330

    chore: frontend listen port

--- scripts/setup-pm2-instances.sh
+++ scripts/setup-pm2-instances.sh
@@ -1,2 +1,2 @@
 ssh $zxc pm2 start --name rasa-backend --cwd /home/yooneskh/rasa/backend/ deno -- task start
-ssh $zxc pm2 start --name rasa-frontend --cwd /home/yooneskh/rasa/frontend/ deno -- task start
+ssh $zxc PORT=48503 pm2 start --name rasa-frontend --cwd /home/yooneskh/rasa/frontend/ deno -- task start
